3bf8f47de38e79fde5945a7e320361548343826d
alex-monro <alexmonro546@gmail.com>
2026-02-02 20:49:30 -0800

    Day 4

---
+++ assets/js/color-scroll-top.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+const scrollButton = document.getElementById('scroll-top');
+scrollButton.style.border = "none";
+
+scrollButton.style.fill = "yellow";
+scrollButton.style.stroke = "none";
+
+
+})
